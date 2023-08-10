5d868fc5979340b43086d574b8b07e4b16c90a56
Steve Lin <steve.lin@fullerton.com.tw>
2023-08-10 21:48:03 +0800

    移除counterSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clientReducer from "../features/client/clientSlice";
-import counterReducer from "../features/counter/counterSlice";
 import orderReducer from "../features/order/orderSlice";
 import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     order: orderReducer,
     client: clientReducer,
     auth: authReducer,
