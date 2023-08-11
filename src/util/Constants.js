260f176dfe9b670e379c209ccf3ac1bbd3bd3162
Steve Lin <steve.lin@fullerton.com.tw>
2023-08-11 23:31:25 +0800

    調整production環境時 api url的位置(從cyclic改到 render.com)

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,12 +1,14 @@
 const prod = {
   url: {
     // API_URL: "https://flower-shop-back-end.herokuapp.com",
-    API_URL: "https://real-erin-dove-sari.cyclic.app/",
+    // API_URL: "https://real-erin-dove-sari.cyclic.app/",
+    API_URL: "https://flower-shop-backend-9cq3.onrender.com",
   },
 };
 const dev = {
   url: {
     API_URL: "http://localhost:8080",
+    // API_URL: "https://flower-shop-backend-9cq3.onrender.com",
   },
 };
 export const Config = process.env.NODE_ENV === "development" ? dev : prod;
