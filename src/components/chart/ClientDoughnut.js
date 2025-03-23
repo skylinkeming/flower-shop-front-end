b3eecff5ac3dbafb4c4539363e9114520611c502
Steve Lin <germinglin@yahoo.com.tw>
2025-03-23 20:25:45 +0800

    remove unused package

--- src/components/chart/ClientDoughnut.js
+++ src/components/chart/ClientDoughnut.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
